ba5a9a51ab93528a51a1b1e0631b4a8875dc3dbb
Haythem Bekir <65097270+haythembekir20@users.noreply.github.com>
2021-10-30 15:34:42 +0100

    first commit

---
+++ src/main/java/com/example/demo/JwtGeneratorApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.demo.entities.Role;
+import com.example.demo.entities.User;
+import com.example.demo.service.UserService;
+
+@SpringBootApplication
+public class JwtGeneratorApplication {
+	
+	@Autowired
+	UserService userService;
+	
+	@Bean
+	BCryptPasswordEncoder getBCE() {
+		return new BCryptPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtGeneratorApplication.class, args);
+	}
+	
+
+
+}
